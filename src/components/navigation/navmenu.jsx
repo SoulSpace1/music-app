1e732e2421fbfe2d1b47d99374fc961446465d9b
svetlana <salvatore1864n@yandex.ru>
2023-01-17 19:12:21 +0300

    Update navmenu.jsx

--- src/components/navigation/navmenu.jsx
+++ src/components/navigation/navmenu.jsx
@@ -7,15 +7,9 @@ export function NavMenu({ user, onAuthButtonClick }){
     return (
         <div className={style.nav__menu}>
             <ul className={style.menu__list}>
-
-                <NavLink  to="/"
-              className={({ isActive }) => {
-                  [activeClassName]: isActive,
-                })
-            }>Главное</NavLink>
-                <li className={style.menu__item}><a href="http://" className={style.menu__link}>Главное</a></li>
-                <li className={style.menu__item}><a href="http://" className={style.menu__link}>Мой плейлист</a></li>
-                <li className={style.menu__item}><a href="http://" className={style.menu__link}>Войти</a></li>
+                <li className={style.menu__item}><NavLink to="/"className={style.menu__link}>Главное</NavLink></li>
+                <li className={style.menu__item}><NavLink to="/my-playlist" className={style.menu__link}>Мой плейлист</NavLink></li>
+                <li className={style.menu__item}><NavLink to="/login" className={style.menu__link}>Войти</NavLink></li>
             </ul>
 
                 <button onClick={onAuthButtonClick}>
